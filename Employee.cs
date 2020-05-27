6bf1089814caaa3ee5a752d10ddd1977dd6f227b
Subodh Singh <SubodhSingh@tsys.com>
2020-05-28 02:13:07 +0530

    Test-123

---
+++ Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace API.Models
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string Gender;
+        public long salary;
+        public Int32 Age;
+        public Int32 Id;
+
+        public List<Employee> GetallEmployees()
+        {
+            string[] Alllines = File.ReadAllLines(@"C:\Users\subodhsingh\Desktop\WebAPIDB\Employee.txt");
+            List<Employee> Emp = new List<Employee>();
+            for (int i = 0;i< Alllines.Length;i++)
+            {
+                string[] getEmployee = Alllines[i].ToString().Split(',');
+                Employee e = new Employee();
+                e.FirstName = getEmployee[0];
+                e.LastName = getEmployee[1];
+                e.Gender = getEmployee[2];
+                e.salary =Convert.ToInt64( getEmployee[3]);
+                e.Age =  Convert.ToInt32(getEmployee[4]);
+                e.Id = Convert.ToInt32(getEmployee[5]);
+                Emp.Add(e);
+            }
+            return Emp;
+            
+        }
+
+        public void Addemployee(Employee emp)
+        {
+            try
+            {
+                String NewEmployee = emp.FirstName + "," + emp.LastName + "," + emp.Gender + "," + emp.salary + "," + emp.Age + "," + emp.Id;
+                File.AppendAllText(@"C:\Users\subodhsingh\Desktop\WebAPIDB\Employee.txt",Environment.NewLine+ NewEmployee);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public int UpdateEmployee(Employee emp)
+        {
+            try
+            {
+                string[] Alllines = File.ReadAllLines(@"C:\Users\subodhsingh\Desktop\WebAPIDB\Employee.txt");
+                List<Employee> Emp = new List<Employee>();
+                for (int i = 0; i < Alllines.Length; i++)
+                {
+                   
+                    string[] getEmployee = Alllines[i].ToString().Split(',');
+                    if (Convert.ToInt32(getEmployee[5])==emp.Id)
+                    { 
+                        Alllines[i]= emp.FirstName + "," + emp.LastName + "," + emp.Gender + "," + emp.salary + "," + emp.Age + "," + emp.Id;
+                        File.WriteAllLines(@"C:\Users\subodhsingh\Desktop\WebAPIDB\Employee.txt",Alllines);
+                        return 1;
+                        break;
+                    }
+                           
+
+                  
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        //public void DeletEmployee(int id)
+        //{
+        //    try
+        //    {
+        //        String NewEmployee = emp.FirstName + "," + emp.LastName + "," + emp.Gender + "," + emp.salary + "," + emp.Age + "," + emp.Id;
+        //        File.AppendAllText(@"C:\Users\subodhsingh\Desktop\WebAPIDB\Employee.txt", Environment.NewLine + NewEmployee);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+
+        //}
+    }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace API.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Models.Employee> GetAllEmployees()
+        {
+            Models.Employee emp = new Models.Employee();
+            return emp.GetallEmployees();
+        }
+        [HttpGet]
+        public HttpResponseMessage GetAllEmployees(int id)
+        {
+            Models.Employee emp = new Models.Employee();
+            IEnumerable<Models.Employee> foundemp= emp.GetallEmployees().Where(x => x.Id == id);
+            if(foundemp.Count()>0)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, foundemp);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "No Employee Was found with this ID.");
+            }
+           
+        }
+        [HttpPost]
+        public HttpResponseMessage AddEmployee([FromBody]Models.Employee Employee)
+        {
+            try
+            {
+                Models.Employee emp = new Models.Employee();
+                emp.Addemployee(Employee);
+                return Request.CreateResponse(HttpStatusCode.Created, "Employee Added Successfully.");
+
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.ExpectationFailed, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateEmp([FromBody]Models.Employee Employee)
+        {
+            Models.Employee emp = new Models.Employee();
+            emp.UpdateEmployee(Employee);
+            return Request.CreateResponse(HttpStatusCode.Created, "Employee updated Successfully.");
+        }
+
+
+    }
+}
